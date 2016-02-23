438b5071e459031fe9a10ac1ef316a9b7d7e7468
Rajind Karunaratne <rajindkarunaratne@yahoo.com>
2016-02-23 12:23:26 +0000

    Added Translation

--- polygon_reader/src/src/polygon_test/Drawing.java
+++ polygon_reader/src/src/polygon_test/Drawing.java
@@ -2,11 +2,12 @@ package src.polygon_test;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+import math.geom2d.AffineTransform2D;
 import math.geom2d.Point2D;
 import math.geom2d.polygon.*;
 
@@ -25,15 +26,51 @@ public class Drawing extends JFrame{
         Graphics2D g2d = (Graphics2D) g;
      
         Polygon2D x = new SimplePolygon2D();
-		Point2D p1 = new Point2D(-50, 10);
-		Point2D p2 = new Point2D(100, 150);
-		Point2D p3 = new Point2D(200, 20);
+		Point2D p1 = new Point2D(-50, -100);
+		Point2D p2 = new Point2D(100, 0);
+		Point2D p3 = new Point2D(0, 100);
+		
+		Polygon2D y = new SimplePolygon2D();
+		Point2D p4 = new Point2D(-100,50);
+		Point2D p5 = new Point2D(-75,100);
+		Point2D p6 = new Point2D(0,20);
 		
 		x.addVertex(p1);
 		x.addVertex(p2);
 		x.addVertex(p3);
-	    x.draw(g2d);
-	     
+		
+		y.addVertex(p4);
+		y.addVertex(p5);
+		y.addVertex(p6);
+		
+		ArrayList<Polygon2D> polygons = new ArrayList();
+		polygons.add(x);
+		polygons.add(y);
+		
+		double minX = 0;
+		double minY = 0;
+		
+		for(int i = 0; i < polygons.size(); i++){
+			for(int j = 0; j < polygons.get(i).vertexNumber(); j++){
+				if(polygons.get(i).vertex(j).x()<minX){
+					minX = polygons.get(i).vertex(j).x();
+					System.out.println("New X translation : " + minX);
+				}
+				if(polygons.get(i).vertex(j).y()<minY){
+					minY = polygons.get(i).vertex(j).y();
+					System.out.println("New Y translation : " + minY);
+				}
+			}
+		}
+		
+		minX = Math.abs(minX)*2;
+		minY = Math.abs(minY)*2;
+		
+		AffineTransform2D grid_Correction = new AffineTransform2D();
+		AffineTransform2D z = grid_Correction.createTranslation(minX,minY);
+		
+		x.transform(z).draw(g2d);	     
+		y.transform(z).draw(g2d);
  
     }
  
